82d49e6f4c81009cd43b5c81588bbaf9d1c1bee9
Eric Fairon <eric.fairon@gmail.com>
2018-02-11 15:09:55 +0700

    [ADD] 1st commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # vgy
 Simple Post image to vgy.me
+
+
+## vgy API doc
+
+API doc is here https://vgy.me/api
+
+## Usage
+
+```golang
+
+import "github.com/frontware.com/vgy"
+import "fmt"
+
+func main () {
+
+    vgy.UserKey = "PUT YOUR VGY USER KEY HERE"
+    info, err := vgy.UploadImageFile("image.png")
+    if err!=nil {
+        fmt.Fatal("Upload failed ",err)
+    }
+    fmt.Println("Upload success")
+    fmt.Println(info.String())
+
+}
+
+
+```
---
+++ vgy.go
@@ -0,0 +1,103 @@
+package vgy
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+//go:generate ffjson $GOFILE
+
+// Response returned information from vgy.me after posting an image
+// ffjson: noencoder
+type Response struct {
+	Error     bool
+	URL       string
+	Image     string
+	Extension string
+	Filesize  int64
+	Delete    string
+}
+
+// Print returns string formated Response
+func (r Response) Print() string {
+	result := "URL: " + r.URL + "\n"
+	result += "Image: " + r.Image + "." + r.Extension + "\n"
+	result += fmt.Sprintf("Filesize: %d", r.Filesize)
+	return result
+}
+
+// UserKey must be defined at https://vgy.me/account/details
+var UserKey string
+
+const urlUpload = "https://vgy.me/upload"
+
+// UploadImageFile uploads an image (.png, .jpg) to vgy.me
+func UploadImageFile(fileName string) (response Response, err error) {
+	// Prepare a form that you will submit to that URL.
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	// Add your image file
+	f, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	fw, err := w.CreateFormFile("file", fileName)
+	if err != nil {
+		return
+	}
+	if _, err = io.Copy(fw, f); err != nil {
+		return
+	}
+	if UserKey != "" {
+		// Add the other fields
+		if fw, err = w.CreateFormField("userkey"); err != nil {
+			return
+		}
+		if _, err = fw.Write([]byte(UserKey)); err != nil {
+			return
+		}
+	}
+	// Don't forget to close the multipart writer.
+	// If you don't close it, your request will be missing the terminating boundary.
+	w.Close()
+
+	// Now that you have a form, you can submit it to your handler.
+	req, err := http.NewRequest("POST", urlUpload, &b)
+	if err != nil {
+		return
+	}
+	// Don't forget to set the content type, this will contain the boundary.
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	// Submit the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	// Check the response
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf("bad status: %s", res.Status)
+	}
+	defer res.Body.Close()
+	respBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	err = response.UnmarshalJSON(respBody)
+	if response.Error {
+		err = errors.New("error")
+	}
+
+	return
+
+}
